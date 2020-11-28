96389f54d24d9db48db47f35f89c862da559e20f
zengfanrong <zengfanrong@vvic.com>
2020-11-28 14:56:14 +0800

    feat: 修正参数

--- gcmpf_no_merges.sh
+++ gcmpf_no_merges.sh
@@ -5,14 +5,10 @@ echo -e "\n"
 git log $1 --first-parent --no-merges --pretty="%C(Yellow)%h %C(Cyan)%an: %C(reset)%s %C(reset)%ad" | awk -F ' ' '{$1="";print $0}' > /tmp/select_commit
 git log HEAD --first-parent --no-merges --pretty="%C(Yellow)%h %C(Cyan)%an: %C(reset)%s %C(reset)%ad" | awk -F ' ' '{$1="";print $0}' > /tmp/head_commit
 
-echo -e "\n"
 echo "only in select commit:"
-echo -e "\n"
 
-grep -vxf /tmp/head_commit /tmp/select_commit
+grep -vxfF /tmp/head_commit /tmp/select_commit
 
-echo -e "\n"
 echo "only in HEAD commit:"
-echo -e "\n"
 
-grep -vxf /tmp/select_commit /tmp/head_commit
+grep -vxfF /tmp/select_commit /tmp/head_commit
